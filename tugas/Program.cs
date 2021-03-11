5d7143aa5444cff58ea255bde3e9b3e53e50f4c1
Bombong <coroultima2506@gmail.com>
2021-03-11 14:05:25 +0700

    Update menu system

--- tugas/Program.cs
+++ tugas/Program.cs
@@ -1,8 +1,20 @@
-﻿class Calculator
+﻿using System;
+class Calculator
 {
     static void Main(string[] args)
     {
+        Console.Write("Inputkan nilai a: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Inputkan nilai b: ");
+        int b = int.Parse(Console.ReadLine());
 
+        Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
+        Console.WriteLine("Hasil Pengurangan : {0} - {1} = {2}", a, b, Pengurangan(a, b));
+        Console.WriteLine("Hasil Pembagian : {0} / {1} = {2}", a, b, Pembagian(a, b));
+        Console.WriteLine("Hasil Perkalian : {0} * {1} = {2}", a, b, Perkalian(a, b));
+
+        Console.WriteLine("\nTekan sembarang key untuk keluar");
+        Console.ReadKey();
     }
     static int Penambahan(int a, int b)
     {
